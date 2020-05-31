b5ab7b541f5f65a49ed37910b8b13f451e1cd0ca
Akash Gupta <akash8.gupta@paytm.com>
2020-05-31 15:16:23 +0530

    fetched anime to cards

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -10,17 +10,21 @@ const BASE_URL = 'https://api.jikan.moe/v3/search/anime?q=<query>'
 
 export default function Body(){
 
-    useEffect(()=>{
-        // .fetch('')
-    },[])
-
-    const body = useSelector( state => state.body)
+    // const body = useSelector( state => state.body)
     const dispach = useDispatch()
     const [data,setData] = useState({ animeList: [], isLoading: true})
+    const limit = 16
+
+    useEffect(()=>{
+        fetch(`${BASE_URL}&limit=${limit}`)
+        .then ( res => res.json())
+        .then ( data => setData({animeList: data.results, isLoading:0}))
+    },[])
 
     function increase(){
         dispach({type: 'INCREASE_AGE' , by: 10})
     }
+
     return <div className={`${style.body}`}>
         {
             data.isLoading &&
@@ -29,12 +33,16 @@ export default function Body(){
         {
             !data.isLoading &&
             <div className={`flex ${style.cards}`}>
-                <Card />
-                <Card />
-                <Card />
+                {
+                    data.animeList.map( ( anime,i ) => {
+                        return <React.Fragment key = {anime.mal_id}>
+                            <Card data={anime} />
+                        </React.Fragment>
+                    })
+                }
             </div>
         }
-        Some content here age : { body.age }
+        {/* Some content here age : { body.age } */}
         <button onClick={increase}>increase</button>
     </div>
 }
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import * as style from './style.module.scss'
 
-export default function Card(){
-    return <div className={style.card}>
+export default function Card( props ){
+    
+    const  anime  = props.data;
+
+    return <div className={style.card}  style={{backgroundImage: `url(${anime.image_url})`}}>
         <div className={style.name}>
-            Hunter X Hunter
+            { anime.title }
         </div>
     </div>
 }
